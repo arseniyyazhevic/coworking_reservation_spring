3cebf171b1c191732f7a8db959468738a750918d
Arseniy Yazhevich <arseniy.yazhevich@gmail.com>
2025-04-03 16:22:37 +0300

    reformat code

--- src/main/java/course/dao/BookingDAO.java
+++ src/main/java/course/dao/BookingDAO.java
@@ -3,6 +3,7 @@ package course.dao;
 import course.entity.Booking;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transaction;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
@@ -17,17 +18,17 @@ public class BookingDAO {
 
     @Transactional
     public void saveBooking(Booking booking) {
-       entityManager.persist(booking);
+        entityManager.persist(booking);
     }
 
     @Transactional
     public Booking getBookingById(Long id) {
-            return entityManager.find(Booking.class, id);
+        return entityManager.find(Booking.class, id);
     }
 
     @Transactional
     public List<Booking> getAllBookings() {
-            return entityManager.createQuery("from Booking", Booking.class).getResultList();
+        return entityManager.createQuery("from Booking", Booking.class).getResultList();
     }
 
     @Transactional
