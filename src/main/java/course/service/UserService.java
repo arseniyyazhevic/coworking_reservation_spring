a876589b4fec746dfd8961d39e08ef5fe44f6f58
Arseniy Yazhevich <arseniy.yazhevich@gmail.com>
2025-04-04 15:09:45 +0300

    remove ui

--- src/main/java/course/service/UserService.java
+++ src/main/java/course/service/UserService.java
@@ -3,8 +3,6 @@ package course.service;
 import course.entity.roles.Admin;
 import course.entity.roles.Customer;
 import course.entity.roles.User;
-import course.ui.ConsoleInput;
-import course.ui.ConsoleOutput;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,17 +15,6 @@ public class UserService {
         } else {
             return new Customer();
         }
-    }
-
-    public User getUserAndSetLogin(ConsoleInput consoleInput) {
-        User user = consoleInput.getUserRoleInput("Please choose your role Customer(1) or Admin(2): ");
-        String userLoginInput = consoleInput.getUserLoginInput(("If you want to Exit enter 3 or write down your login (five and more characters): "));
-        if (userLoginInput.equals("exit") || userLoginInput.equals("3")) {
-            return null;
-        }
-        ConsoleOutput.println(userLoginInput);
-        user.setLogin(userLoginInput);
-        return user;
     }
 
 }
