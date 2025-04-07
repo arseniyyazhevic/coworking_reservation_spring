8ceb92b21197b3665483ce2adaa0b014fafb0791
Arseniy Yazhevich <arseniy.yazhevich@gmail.com>
2025-04-07 14:44:03 +0300

    add exception handler

---
+++ src/main/java/course/controller/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package course.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleAllExceptions(Exception ex, Model model) {
+        model.addAttribute("errorMessage",
+                "An unexpected error occurred" + ex.getMessage());
+        return "error";
+    }
+}
