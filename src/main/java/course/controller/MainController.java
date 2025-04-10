781262ec0283c5259b472fdba03b29e5fc2179a4
Arseniy Yazhevich <arseniy.yazhevich@gmail.com>
2025-04-10 17:06:25 +0300

    rename main controller add mapping on /login

--- src/main/java/course/controller/MainController.java
+++ src/main/java/course/controller/MainController.java
@@ -1,17 +1,21 @@
 package course.controller;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class HomeController {
+public class MainController {
 
     @GetMapping
     public String showForm() {
         return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
 }
