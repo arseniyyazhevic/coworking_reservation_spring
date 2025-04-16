c7f1787a947437cfbbf5a39f2c598493fc671bb7
Arseniy Yazhevich <arseniy.yazhevich@gmail.com>
2025-04-16 17:35:18 +0300

    add undo operation in logic

--- src/main/java/course/service/BookingService.java
+++ src/main/java/course/service/BookingService.java
@@ -1,22 +1,29 @@
 package course.service;
 
 import course.entity.BookingEntity;
+import course.entity.BookingHistoryEntity;
+import course.repository.BookingHistoryRepository;
 import course.repository.BookingRepository;
+import course.service.memento.BookingHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.*;
 
 @Service
 public class BookingService {
 
     private final BookingRepository bookingRepository;
+
+    private final BookingHistoryService bookingHistoryService;
     @Autowired
-    public BookingService(BookingRepository bookingRepository) {
+    public BookingService(BookingRepository bookingRepository, BookingHistoryService bookingHistoryService) {
         this.bookingRepository = bookingRepository;
+        this.bookingHistoryService = bookingHistoryService;
     }
 
     @Cacheable(value="bookings", key="#id")
@@ -41,6 +48,31 @@ public class BookingService {
     @Cacheable(value = "allBookings")
     public List<BookingEntity> getAllBookings() {
         return bookingRepository.findAll();
+    }
+
+
+    public void updateBookingWithHistory(Long bookingId, String newCustomerName, String newInterval, LocalDate newDate) {
+        BookingEntity booking = bookingRepository.findById(bookingId).orElseThrow();
+        bookingHistoryService.saveSnapshot(booking);
+
+        booking.setCustomerName(newCustomerName);
+        booking.setStartAndEndOfBookingTime(newInterval);
+        booking.setDate(newDate);
+        bookingRepository.save(booking);
+    }
+
+    public void undoBookingChange(Long bookingId) {
+        BookingEntity booking = bookingRepository.findById(bookingId).orElseThrow();
+        Optional<BookingHistoryEntity> snapshotOpt = bookingHistoryService.getLastSnapshot(bookingId);
+
+        if (snapshotOpt.isPresent()) {
+            BookingHistoryEntity snapshot = snapshotOpt.get();
+            booking.setCustomerName(snapshot.getCustomerName());
+            booking.setStartAndEndOfBookingTime(snapshot.getStartAndEndOfBookingTime());
+            booking.setDate(snapshot.getDate());
+            booking.setCoworkingSpaceEntity(snapshot.getCoworkingSpaceEntity());
+            bookingRepository.save(booking);
+        }
     }
 
 }
